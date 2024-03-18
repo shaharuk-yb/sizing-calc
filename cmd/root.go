f8bd510db60b36196ccb5d5b93676bce36564f45
shaharuk-yb <sshaikh@yugabyte.com>
2024-03-18 15:09:58 +0530

    init3

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,5 @@
 /*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
+Copyright © 2024 shaharuk-yb <sshaikh@yugabyte.com>
 */
 package cmd
 
@@ -10,18 +9,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "sizing-calc",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "sizing recommendation for yugabyte database",
+	Long: `Sizing calculator helps users by providing recommendations for yugabyte database. For example:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+What size of yugabyte db cluster/universe should they create for specific set of requirements.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -47,5 +41,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
--- cmd/sizer.go
+++ cmd/sizer.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+Copyright © 2024 shaharuk-yb <sshaikh@yugabyte.com>
 */
 package cmd
 
@@ -33,36 +33,39 @@ NOTE: the recommendations can change based on the target yugabytedb version`,
 
 func init() {
 	rootCmd.AddCommand(sizerCmd)
+
+	// Here you will define your flags and configuration settings.
 	sizerCmd.Flags().IntP("tables", "t", 1, "number of tables")
 	sizerCmd.Flags().IntP("select-throughput", "s", 1, "desired select throughput")
 	sizerCmd.Flags().IntP("insert-throughput", "i", 1, "desired isnert throughput")
 	sizerCmd.Flags().StringP("target-yb-version", "y", "2.20", "target yugabyte db version")
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// sizerCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// sizerCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 func switching(targetYbVersion string) {
 	filePath := "resources/yb_" + strings.ReplaceAll(targetYbVersion, ".", "_") + ".txt"
 
-	checkFileExistsOnRepo(filePath)
-	/*	fmt.Println(filePath)
+	remoteFileExists, contents := checkFileExistsOnRemoteRepo(filePath)
 
+	if remoteFileExists {
+		// print the contents of the file
+		fmt.Println(contents)
+	} else {
+		//check if local file exists
 		isFileExist := checkLocalFileExists(filePath)
 
 		if isFileExist {
-			fmt.Println("file exist")
+			fmt.Println("file exist locally")
+			// read the file
+			cont, err := os.ReadFile(filePath)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(string(cont))
 		} else {
 
-			fmt.Println("file doesn't exist")
-		}*/
-
+			fmt.Println("file doesn't exist locally")
+		}
+	}
 }
 
 func checkLocalFileExists(filePath string) bool {
@@ -70,20 +73,25 @@ func checkLocalFileExists(filePath string) bool {
 	return !errors.Is(err, os.ErrNotExist)
 }
 
-func checkFileExistsOnRepo(fileName string) bool {
-	resp, err := http.Get("https://raw.githubusercontent.com/shaharuk-yb/sizing-calc/init/" + fileName)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
+func checkFileExistsOnRemoteRepo(fileName string) (bool, string) {
+	remotePath := "https://raw.githubusercontent.com/shaharuk-yb/sizing-calc/init/" + fileName
+	resp, _ := http.Get(remotePath)
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(resp.Body)
 
 	if resp.StatusCode != 200 {
-		fmt.Println("file does not exist on remote location")
+		fmt.Println("File does not exist on remote location")
+		return false, ""
+	} else {
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			panic(err)
+		}
+		return true, string(body)
 	}
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(body))
-	return true
 }
